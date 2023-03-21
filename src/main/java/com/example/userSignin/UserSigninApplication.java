7f2d81ba093410aceb691824589aa3277af87172
Hemanth912 <116891140+Hemanth912@users.noreply.github.com>
2023-03-21 18:51:03 +0530

    Initial commit

---
+++ src/main/java/com/example/userSignin/UserSigninApplication.java
@@ -0,0 +1,13 @@
+package com.example.userSignin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSigninApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserSigninApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/userSignin/UserSigninApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userSignin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSigninApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
