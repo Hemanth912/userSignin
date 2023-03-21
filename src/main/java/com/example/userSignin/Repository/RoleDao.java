cb5ea22681d36acd96dc7d9293a7c418f711aee5
Hemanth912 <116891140+Hemanth912@users.noreply.github.com>
2023-03-21 20:34:44 +0530

    repo commit

---
+++ src/main/java/com/example/userSignin/Repository/RoleDao.java
@@ -0,0 +1,10 @@
+package com.example.userSignin.Repository;
+
+import com.springrolejwt.model.Role;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleDao extends CrudRepository<Role, Long> {
+    Role findRoleByName(String name);
+}
